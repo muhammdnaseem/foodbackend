9999791ff622ccca61950f2519a1416010e52e0c
muhammd naseem <muhammadnaseem0342@gmail.com>
2024-10-29 11:30:19 +0500

    d

--- models/userModel.js
+++ models/userModel.js
@@ -22,12 +22,12 @@ const userSchema = new mongoose.Schema({
 }, { minimize: false });
 
 // Pre-save middleware to enforce password requirement conditionally
-userSchema.pre('save', function (next) {
-    if (!this.googleId && !this.password) {
-        return next(new Error('Password is required for non-OAuth users.'));
-    }
-    next();
-});
+// userSchema.pre('save', function (next) {
+//     if (!this.googleId && !this.password) {
+//         return next(new Error('Password is required for non-OAuth users.'));
+//     }
+//     next();
+// });
 
 // Export the model safely (only register if not already registered)
 const userModel = mongoose.models.user || mongoose.model('user', userSchema);
